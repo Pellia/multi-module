d3de9e33aa8c8be8b1f65487f6bbff735ca19fe9
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-01-29 15:33:41 +0100

    update

--- modules/storage.js
+++ modules/storage.js
@@ -1,6 +1,14 @@
 // Store Data
 export function storeData(data, id) {
     const storedArray = JSON.parse(localStorage.getItem("myArray")) || [];
-    const newArray = [{ id: id, title: data.title, image: data.image, price: data.price }, ...storedArray];
+    const newArray = [
+        {
+            id: id,
+            title: data.title,
+            image: data.image,
+            price: data.price,
+        },
+        ...storedArray,
+    ];
     localStorage.setItem("myArray", JSON.stringify(newArray));
 }
